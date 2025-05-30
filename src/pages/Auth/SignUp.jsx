3419b4d2741b23d2bc8c90121cae2854855bcfe9
Arunav Singh <arunavsingh999@gmail.com>
2025-05-30 19:53:45 +0530

    completed profile picture upload

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/Inputs/Input";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+
+import { validateEmail } from "../../utils/helper";
 
 export default function SignUp() {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const handleSignUp = async (e) => {};
   return (
-    <div>
-      SignUp
-    </div>
-  )
+    <>
+      <AuthLayout>
+        <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+          <h3 className="text-xl font-semibold text-black">
+            Create an Account
+          </h3>
+          <p className="text-xs text-slate-700 mt-[5px] mb-6">
+            Join us today by entering your details below.
+          </p>
+
+          <form onSubmit={handleSignUp}>
+            <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Input
+                value={fullName}
+                onChange={({ target }) => setFullName(target.value)}
+                label="Full Name"
+                placeholder="John Doe"
+                type="text"
+              />
+
+              <Input
+                id="email"
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+                label="Email Address"
+                placeholder="john@example.com"
+                type="text"
+              />
+
+              <div className="col-span-2">
+                <Input
+                  id="password"
+                  value={password}
+                  onChange={({ target }) => setPassword(target.value)}
+                  label="Password"
+                  placeholder="min 8 characters"
+                  type="password"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </AuthLayout>
+    </>
+  );
 }
