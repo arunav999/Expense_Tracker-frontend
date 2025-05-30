59663e51896bdc365c3b74c7eaad9351b16f7cc0
Arunav Singh <arunavsingh999@gmail.com>
2025-05-30 15:34:51 +0530

    added custom inputs

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+
+import Input from "../../components/Inputs/Input";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e)=> {
+
+  }
+
   return (
     <>
       <AuthLayout>
@@ -9,6 +23,16 @@ export default function Login() {
           <p className="text-xs text-slate-700 mt-[5px] mb-6">
             Please enter your details to login
           </p>
+
+          <form onSubmit={handleLogin}>
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+          </form>
         </div>
       </AuthLayout>
     </>
