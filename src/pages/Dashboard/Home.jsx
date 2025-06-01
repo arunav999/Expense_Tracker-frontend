07b83f79912840c80951e6e2d46ac33584107742
Arunav Singh <arunavsingh999@gmail.com>
2025-06-01 18:11:36 +0530

    dashboard api added

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -1,12 +1,63 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { IoMdCard } from "react-icons/io";
+
+import InfoCard from "../../components/Cards/InfoCard";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
+
 import { useUserAuth } from "../../hooks/useUserAuth";
+
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 export default function Home() {
   useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Something went wrong. Please try again.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+
+    return () => {};
+  }, []);
+
   return (
     <>
       <DashboardLayout activeMenu="Dashboard">
-        <div className="my-5 mx-auto">Home</div>
+        <div className="my-5 mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <InfoCard
+              icon={<IoMdCard />}
+              label="Total Balance"
+              value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+              color="bg-primary"
+            />
+          </div>
+        </div>
       </DashboardLayout>
     </>
   );
