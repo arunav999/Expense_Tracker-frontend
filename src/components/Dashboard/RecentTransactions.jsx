3731ea29acfe0ac36e1f9535b6d6e42503be8ea1
Arunav Singh <arunavsingh999@gmail.com>
2025-06-01 19:53:23 +0530

    designed recent transaction

--- src/components/Dashboard/RecentTransactions.jsx
+++ src/components/Dashboard/RecentTransactions.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { LuArrowRight } from "react-icons/lu";
 
-const RecentTransactions = ({transactions, onSeeMore}) => {
+const RecentTransactions = ({ transactions, onSeeMore }) => {
   return (
     <>
-      
-    </>
-  )
-}
+      <div className="card">
+        <div className="flex items-center justify-between">
+          <h5 className="text-lg">Recent Transactions</h5>
 
-export default RecentTransactions
+          <button className="card-btn" onClick={onSeeMore}>
+            See All <LuArrowRight className="text-base" />
+          </button>
+        </div>
+
+        <div className="mt-6"></div>
+      </div>
+    </>
+  );
+};
+
+export default RecentTransactions;
